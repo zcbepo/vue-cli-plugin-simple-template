b24a76fda9c25110d65475595bc83085ac5c5c14
Cheng Zou <cheng.zou@pactera.com>
2020-07-03 16:55:48 +0800

    add scripts

--- generator/index.js
+++ generator/index.js
@@ -3,6 +3,11 @@ module.exports = (api) => {
         dependencies: {
             'axios': '^0.19.2',
             'element-ui': '^2.4.5'
+        },
+        scripts: {
+            'serve:uat': 'vue-cli-service serve --mode=uat',
+            'serve:prod': 'vue-cli-service serve --mode=production',
+            'build:uat': 'vue-cli-service build --mode=uat'
         }
     })
 
